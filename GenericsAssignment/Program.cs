866aab06fe49a189db1d6cf5615e583f93b745ce
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-02 18:50:01 +0530

    [Madhavee] Add method to find maximum of integer

--- GenericsAssignment/Program.cs
+++ GenericsAssignment/Program.cs
@@ -11,6 +11,15 @@ namespace GenericsAssignment
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome");
+            //Get the 3 integers from the user
+            Console.WriteLine("Enter first integer number : ");
+            int first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second integer number : ");
+            int second = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third integer number : ");
+            int third = Convert.ToInt32(Console.ReadLine());
+            int maxValue = FindMaximum.FindMaxInteger(first, second, third); // Storing value as maximum which is returned by Method
+            Console.WriteLine($"Maxium integer of ({first},{second},{third}) is : {maxValue}");
             Console.ReadLine();
         }
     }
