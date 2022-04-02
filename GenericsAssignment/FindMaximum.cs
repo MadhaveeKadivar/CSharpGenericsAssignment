29d59074e42fd5fcd8f17fa4d6008d390b573c15
MadhaveeKadivar <madhaveekadivar560@gmail.com>
2022-04-02 20:34:37 +0530

    [Madhavee] Add Generic class and method to find maximum of more than three values

--- GenericsAssignment/FindMaximum.cs
+++ GenericsAssignment/FindMaximum.cs
@@ -8,24 +8,16 @@ namespace GenericsAssignment
 {
     internal class FindMaximum<T> where T : IComparable // Creating a generic class
     {
-        public static T FindMax (T first, T second, T third)// Creating generic Method to find maximum of three having any data type
+        public static T[] SortValue(T[] arr) // creating generic method to sort the values in ascending order
         {
-            //Checking that first is maximum or not
-            if (first.CompareTo(second)>0 && first.CompareTo(third)>0 || first.CompareTo(second)>=0 && first.CompareTo(third)>0 ||first.CompareTo(second)>0 && first.CompareTo(third)>=0)
-            {
-                return first; // Retrun first as maximum
-            }
-            //Checking that second is maximum or not
-            if (second.CompareTo(first)>0 && second.CompareTo(third)>0 || second.CompareTo(first)>=0 && second.CompareTo(third)>0 || second.CompareTo(first)>0 && second.CompareTo(third)>=0)
-            {
-                return second; // Retrun second as maximum
-            }
-            //Checking that third is maximum or not
-            if (third.CompareTo(first)>0 && third.CompareTo(second) >0 || third.CompareTo(first)>=0 && third.CompareTo(second) >0 || third.CompareTo(first)>0 && third.CompareTo(second) >=0)
-            {
-                return third; // Retrun third as maximum
-            }
-            return first; // Return first if all values are same 
-        }       
+            Array.Sort(arr); // Sorting values
+            return arr; // return sorted array
+        }
+        public static T FindMax(params T[] arr)// Creating generic Method to find maximum of three having any data type
+        {
+            var sorted_values = SortValue(arr); // Calling Sortvalue generic method to sort value
+            return sorted_values[sorted_values.Length -1]; // Returning last element as a Maximum
+        }   
+        
     }
 }
